6005dace06417a160644fbe6c0ab0c996983ad78
Timothy Johnson <tjohnson.acts1422@gmail.com>
2020-12-18 17:42:06 -0500

    Added JS functionality instead of HTML/CSS'

--- js/index.js
+++ js/index.js
@@ -17,18 +17,18 @@ function insertNav() {
           <!-- About Button -->
           <li id="tools"><a href="tools.html">Tools</a></li>
           <!-- Tools Button -->
-          <li id="products-services">
-            <a href="products-services.html">Products &amp; Services</a>
-          </li>
-          <!-- Products & Services  Button -->
           <li id="get-started"><a href="get-started.html">Get Started</a></li>
           <!-- Get Started Button -->
         </ul>
       </nav>`;
 
+          //       <li id="products-services">
+          //   <a href="products-services.html">Products &amp; Services</a>
+          // </li>
+          // <!-- Products & Services  Button -->
+
       const main = document.querySelector('main'); 
     //   console.log(main); //checks that the proper element was selected
-
       main.insertAdjacentHTML('afterbegin', navHTML); 
 
 };
@@ -63,24 +63,43 @@ function insertFooter() {
         ></a>
 
         <a
-          class="instagram"
+          class="linkedIn"
           href="https://www.linkedin.com/in/timothy-johnson-ii-49306a92/"
           ><ion-icon class="social-media-icon" name="logo-linkedIn"></ion-icon
         ></a>
       </div>
     </footer>
-    `
+    `;
     const main = document.querySelector('main'); 
     // console.log(main); //checks that proper element is selected
     main.insertAdjacentHTML('afterend', footerHTML); 
     
 }; 
 
-function fillTestimonials() {
+// function fillTestimonials() {
 
+// }; 
+
+const renderFavicon = () => {
+  const favIconHTML = `<link rel="icon" type="image/jpg" href="../img/favicon/mountain-blue-side-CLEAR  mtn-only.png" size=32x32>;`; 
+
+  let head = document.querySelector('head'); 
+
+  head.insertAdjacentHTML('beforeend', favIconHTML);
+  
 }; 
+
+// function getIonIcons() {
+//   const iconsScript = `<script src="https://unpkg.com/ionicons@5.2.3/dist/ionicons.js"></script>`; 
+
+//   const firstScript = document.querySelector("script"); 
+
+//   firstScript.insertAdjacentHTML('beforebegin', iconsScript); 
+
+// }
 
 // INVOKING THESE FUNCTIONS
 
 insertNav(); 
 insertFooter(); 
+renderFavicon();
